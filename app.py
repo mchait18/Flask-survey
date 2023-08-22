470ac5dac11c5891811226392aee567b866780d8
MiriamChait <miriamchait@gmail.com>
2023-08-22 13:26:27 -0400

    step 8 and fs

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey
-
+from surveys import surveys
+CURRENT_SURVEY_KEY = 'current_survey'
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "chickenzarecool21837"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
 
 @app.route('/')
 def home_page():
-    return render_template('home.html', survey = satisfaction_survey)
+       
+    return render_template('which_survey.html', surveys=surveys)
+
+@app.route('/', methods=["POST"])
+def set_survey():
+    survey_id = request.form['survey']
+    survey = surveys[survey_id]
+    session[CURRENT_SURVEY_KEY] = survey_id
+    return render_template('home.html', survey=survey)
+
+@app.route('/begin', methods = ["POST"])
+def start_survey():
+    session["responses"] = []
+    return redirect('questions/0')
 
 @app.route('/questions/<int:q_id>')
 def get_question(q_id):
+    survey_code = session.get(CURRENT_SURVEY_KEY)
+    survey = surveys[survey_code]
+    responses =session.get('responses')
     if responses is None:
         return redirect('/')
     
-    if len(responses) == len(satisfaction_survey.questions):
+    if len(responses) == len(survey.questions):
         return redirect('/thankyou')
 
 
@@ -28,15 +43,20 @@ def get_question(q_id):
         return redirect (f'/questions/{len(responses)}')
            
     return render_template(
-        'questions.html', question=satisfaction_survey.questions[q_id])
+        'questions.html', question=survey.questions[q_id])
         
 
 
 @app.route('/answer', methods=["POST"])
 def get_answer():
+    survey_code = session[CURRENT_SURVEY_KEY]
+    survey = surveys[survey_code]
     answer = request.form['answer']
+    responses = session['responses']
     responses.append(answer)
-    if len(satisfaction_survey.questions) == len(responses):
+    session['responses'] = responses
+
+    if len(survey.questions) == len(responses):
         return redirect('/thankyou')
     else:
         return redirect('/questions/'+str(len(responses)))
